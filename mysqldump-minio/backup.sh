27f70322197ab06e3053bc24075ef29cc51dd94d
Boban Acimovic <boban.acimovic@gmail.com>
2021-01-08 18:14:03 +0100

    use date without dashes

--- mysqldump-minio/backup.sh
+++ mysqldump-minio/backup.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-DBS=$(mysql -h ${MARIADB_HOST} -u root -p${MARIADB_PASSWORD} -e "SHOW DATABASES;" | grep -Ev "(information_schema|performance_schema|mysql|test)")
+DBS=$(mysql -h ${MARIADB_HOST} -u root -p${MARIADB_PASSWORD} -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema|mysql|test)")
 
 for DB in $DBS; do
     echo "dumping ${DB}"
-    DATE=$(date +"%Y-%m-%d")
+    DATE=$(date +"%Y%m%d")
     S3_FILE="${DB}-${DATE}.sql.bz2"
     mysqldump -h ${MARIADB_HOST} -u root -p${MARIADB_PASSWORD} --routines --triggers ${DB} | bzip2 -c | aws --endpoint-url=${S3_ENDPOINT} --no-progress s3 cp - s3://${S3_BUCKET}/${S3_FILE}
 done

3127b021b1a48f9ea00fcc021329024ef586e838
Boban Acimovic <boban.acimovic@gmail.com>
2021-01-08 17:59:46 +0100

    rename mariadb-client-s3 to mysqldump-minio, add backup script

---
+++ mysqldump-minio/backup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+DBS=$(mysql -h ${MARIADB_HOST} -u root -p${MARIADB_PASSWORD} -e "SHOW DBS;" | grep -Ev "(information_schema|performance_schema|mysql|test)")
+
+for DB in $DBS; do
+    echo "dumping ${DB}"
+    DATE=$(date +"%Y-%m-%d")
+    S3_FILE="${DB}-${DATE}.sql.bz2"
+    mysqldump -h ${MARIADB_HOST} -u root -p${MARIADB_PASSWORD} --routines --triggers ${DB} | bzip2 -c | aws --endpoint-url=${S3_ENDPOINT} --no-progress s3 cp - s3://${S3_BUCKET}/${S3_FILE}
+done

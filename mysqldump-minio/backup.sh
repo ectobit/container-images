f1a915bb90f71102ba3480c75ab8934aa6e9bc1d
Boban Acimovic <boban.acimovic@gmail.com>
2021-01-08 18:22:49 +0100

    use underscore instead of dash

--- mysqldump-minio/backup.sh
+++ mysqldump-minio/backup.sh
@@ -7,6 +7,6 @@ DBS=$(mysql -h ${MARIADB_HOST} -u root -p${MARIADB_PASSWORD} -e "SHOW DATABASES;
 for DB in $DBS; do
     echo "dumping ${DB}"
     DATE=$(date +"%Y%m%d")
-    S3_FILE="${DB}-${DATE}.sql.bz2"
+    S3_FILE="${DB}_${DATE}.sql.bz2"
     mysqldump -h ${MARIADB_HOST} -u root -p${MARIADB_PASSWORD} --routines --triggers ${DB} | bzip2 -c | aws --endpoint-url=${S3_ENDPOINT} --no-progress s3 cp - s3://${S3_BUCKET}/${S3_FILE}
 done

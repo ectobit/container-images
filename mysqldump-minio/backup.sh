90f43272e7ba880ac3330b7756a5efa1e6b5f8e4
Alexandre Morozov <almorr@gmail.com>
2022-09-22 14:24:43 +0200

    Update backup.sh
    Use {MARIADB_USER} variable instead of hardcoded user root

--- mysqldump-minio/backup.sh
+++ mysqldump-minio/backup.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-DBS=$(mysql -h ${MARIADB_HOST} -u root -p${MARIADB_PASSWORD} -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema|mysql|test)")
+DBS=$(mysql -h ${MARIADB_HOST} -u {MARIADB_USER} -p${MARIADB_PASSWORD} -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema|mysql|test)")
 
 for DB in $DBS; do
     echo "dumping ${DB}"
     DATE=$(date +"%Y%m%d")
     S3_FILE="${DB}_${DATE}.sql.bz2"
-    mysqldump -h ${MARIADB_HOST} -u root -p${MARIADB_PASSWORD} --routines --triggers ${DB} | bzip2 -c | aws --endpoint-url=${S3_ENDPOINT} --no-progress s3 cp - s3://${S3_BUCKET}/${S3_FILE}
+    mysqldump -h ${MARIADB_HOST} -u {MARIADB_USER} -p${MARIADB_PASSWORD} --routines --triggers ${DB} | bzip2 -c | aws --endpoint-url=${S3_ENDPOINT} --no-progress s3 cp - s3://${S3_BUCKET}/${S3_FILE}
 done
